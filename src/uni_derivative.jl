f8f4e5d8f68e9d44b8173e7eae0c8de84822a9c4
George Shaikovski <george.shaikouski@mobilityhouse.com>
2019-11-26 21:49:49 +0100

    uni_derivative: parametrise

--- src/uni_derivative.jl
+++ src/uni_derivative.jl
@@ -4,15 +4,23 @@ Compute derivative of a univariate function.
 """
 
 abstract type DerivativeMethod end
-struct CentralDiff <: DerivativeMethod end
-struct ComplexDiff <: DerivativeMethod end
 
-function df(::CentralDiff, f, x::Real; h = cbrt(eps()))::Real
-    (f(x + h / 2) - f(x - h / 2)) / h
+struct CentralDiff <: DerivativeMethod
+    h
+    CentralDiff(h = cbrt(eps())) = new(h)
 end
 
-function df(::ComplexDiff, f, x::Real; h = 1e-20)::Real
-    imag(f(x + h * im)) / h
+function df(param::CentralDiff, f, x::Real)::Real
+    (f(x + param.h / 2) - f(x - param.h / 2)) / param.h
+end
+
+struct ComplexDiff <: DerivativeMethod
+    h
+    ComplexDiff(h = 1e-20) = new(h)
+end
+
+function df(param::ComplexDiff, f, x::Real)::Real
+    imag(f(x + param.h * im)) / param.h
 end
 
 end
