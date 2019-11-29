4eb979f6b4d877b14a0bc84c87b43eb84efb9d28
George Shaikovski <george.shaikouski@mobilityhouse.com>
2019-11-29 15:45:51 +0100

    tests.benchmarking: benchmark max grad descent

--- test/benchmarking.jl
+++ test/benchmarking.jl
@@ -1,16 +1,18 @@
-module BenchmarkAlgopt
+module Benchmark
 
 using BenchmarkTools
 
-using Algopt.LocalDescent:
-search,
-LineSearch, StrongBacktracking
-
-
+import Algopt.LocalDescent
 quad0 = x->x'x
 ∇quad0 = x->2x
+@btime LocalDescent.search(LocalDescent.LineSearch(), quad0, [10, 10], [-1, -1])
+@btime LocalDescent.search(LocalDescent.StrongBacktracking(), quad0, ∇quad0, [10, 10], [-1, -1])
 
-@btime search(LineSearch(), quad0, [10, 10], [-1, -1])
-@btime search(StrongBacktracking(), quad0, ∇quad0, [10, 10], [-1, -1])
+import Algopt.FirstOrder
+using Algopt.TestFunctions: Rosenbrock
+ros = Rosenbrock()
+mgd = FirstOrder.MaximumGradientDescent()
+term = FirstOrder.Termination(max_iter = 1e6, ϵ_abs = 1e-6, ϵ_rel = 1e-6, ϵ_grad = 1e-6)
+@btime FirstOrder.search($mgd, $ros.f, $ros.∇f, x_0, term = $term) setup = (x_0 = [rand(-2:.1:2), rand(-2:.1:2)])
 
 end
