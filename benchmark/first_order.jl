f2782adc12bd61e2877c3f8a711902750fa7cccc
George Shaikovski <george.shaikouski@mobilityhouse.com>
2019-11-29 22:17:38 +0100

    benchmark: separate into a directory

---
+++ benchmark/first_order.jl
@@ -0,0 +1,16 @@
+using BenchmarkTools
+
+import Algopt.FirstOrder
+using Algopt.TestFunctions: Rosenbrock
+
+ros = Rosenbrock()
+
+term = FirstOrder.Termination(max_iter = 1e6, ϵ_abs = 1e-6, ϵ_rel = 1e-6, ϵ_grad = 1e-6)
+
+mgd = FirstOrder.MaximumGradientDescent()
+res = @btime FirstOrder.search($mgd, $ros.f, $ros.∇f, x_0, term = $term) setup = (x_0 = [rand(-2:.1:2), rand(-2:.1:2)])
+println(res)
+
+gd = FirstOrder.GradientDescent(α = 1e-2)
+res = @btime FirstOrder.search($gd, $ros.f, $ros.∇f, x_0, term = $term) setup = (x_0 = [rand(-2:.1:2), rand(-2:.1:2)])
+println(res)
---
+++ benchmark/local_descent.jl
@@ -0,0 +1,14 @@
+using BenchmarkTools
+
+import Algopt.LocalDescent
+
+quad0 = x->x'x
+∇quad0 = x->2x
+
+ls = LocalDescent.LineSearch()
+res = @btime LocalDescent.search(ls, quad0, [10, 10], [-1, -1])
+println(res)
+
+sbt = LocalDescent.StrongBacktracking()
+res = @btime LocalDescent.search(sbt, quad0, ∇quad0, [10, 10], [-1, -1])
+println(res)
