51d068ecb378dcfc3ed4b9f123c68245a3ed37f0
George Shaikovski <george.shaikouski@mobilityhouse.com>
2019-12-25 20:44:12 +0100

    plot: add conj grad descent

--- plot/first_order.jl
+++ plot/first_order.jl
@@ -32,3 +32,16 @@ function grad_descent(α)
     plot(contour(x, y, (x, y)->ros.f([x, y]); levels = range(0, 5, step = .1)))
     plot!(res[1,:], res[2,:], linewidth = 3)
 end
+
+function conjugate_grad_descent()
+    cgd = FirstOrder.ConjugateGradientDescent()
+
+    x_0 = [rand(-2:.1:2), rand(-2:.1:2)]
+    res = FirstOrder.search(cgd, ros.f, ros.∇f, x_0, term = term, trace = true)
+    println(res[:, end])
+    println(size(res))
+
+    x = y = range(-2, 2, step = .1)
+    plot(contour(x, y, (x, y)->ros.f([x, y]); levels = range(0, 5, step = .1)))
+    plot!(res[1,:], res[2,:], linewidth = 3)
+end
