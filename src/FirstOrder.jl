0e0dc81d48dfa46ffe35a7f7e0a3f75d1549576d
George Shaikovski <george.shaikouski@mobilityhouse.com>
2019-12-25 15:49:54 +0100

    FirstOrder: encapsulate descent parameters in descent_step closure

--- src/FirstOrder.jl
+++ src/FirstOrder.jl
@@ -32,14 +32,11 @@ mutable struct TerminationConditions
         ∇fx_next->norm(∇fx_next) < term.ϵ_grad)
 end
 
-abstract type FirstOrderMethods end
-
-function search(params::FirstOrderMethods, f, ∇f, x_0; term = Termination(), trace = false)
+function descent_until(term::Termination, descent_method, f, ∇f, X, trace)
     term_cond = TerminationConditions(term)
-    X = x_0
     for _ = 1:term.max_iter
         x = X[:, end]
-        x_next = descent_step(params, f, ∇f, x)
+        x_next = descent_method(f, ∇f, x)
         fx, fx_next, ∇fx_next = f(x), f(x_next), ∇f(x_next)
         if term_cond.abs(fx, fx_next) ||
             term_cond.rel(fx, fx_next) ||
@@ -60,9 +57,19 @@ function search(params::FirstOrderMethods, f, ∇f, x_0; term = Termination(), t
     X
 end
 
+abstract type FirstOrderMethods end
+
+abstract type ProblemState end
+
+# Maximum Gradient Descent
+
 struct MaximumGradientDescent <: FirstOrderMethods end
 
-function descent_step(params::MaximumGradientDescent, f, ∇f, x)
+function search(::MaximumGradientDescent, f, ∇f, x_0; term = Termination(), trace = false)
+    descent_until(term, descent_step_maxgd, f, ∇f, x_0, trace)
+end
+
+function descent_step_maxgd(f, ∇f, x)
     """
     Maximum Gradient Descent
 
@@ -70,21 +77,28 @@ function descent_step(params::MaximumGradientDescent, f, ∇f, x)
     where f(x) in the direction of maximum descent is at its local
     minimum.
     """
-    d = direction(params, ∇f, x)
+    d = direction_maxgd(∇f, x)
     search(LineSearch(), f, x, d)
 end
 
-function direction(params::MaximumGradientDescent, ∇f, x)
+function direction_maxgd(∇f, x)
     g = ∇f(x)
     -g / norm(g)
 end
 
+# Gradient Descent
+
 struct GradientDescent <: FirstOrderMethods
     α
-    GradientDescent(; α = 1) = new(α)
+    GradientDescent(; α = 0.001) = new(α)
 end
 
-function descent_step(params::GradientDescent, f, ∇f, x)
+function search(params::GradientDescent, f, ∇f, x_0; term = Termination(), trace = false)
+    descent_step = (f, ∇f, x)->descent_step_gd(params.α, f, ∇f, x)
+    descent_until(term, descent_step, f, ∇f, x_0, trace)
+end
+
+function descent_step_gd(α, f, ∇f, x)
     """
     Gradient Descent
     Algorithm 5.1
@@ -92,7 +106,7 @@ function descent_step(params::GradientDescent, f, ∇f, x)
     A step of a gradient descent algorithm with fixed learning rate α.
     """
     g = ∇f(x)
-    x - params.α * g
+    x - α * g
 end
 
 end # module
--- test/first_order.jl
+++ test/first_order.jl
@@ -7,9 +7,11 @@ using Test
 using LinearAlgebra
 
 using Algopt.FirstOrder:
-search, descent_step, direction,
 Termination,
-MaximumGradientDescent, GradientDescent
+MaximumGradientDescent, GradientDescent,
+search,
+descent_step_maxgd, direction_maxgd,
+descent_step_gd
 
 quad0 = x->x'x
 ∇quad0 = x->2x
@@ -19,28 +21,29 @@ quad3 = x->(x - [3, 3])' * (x - [3, 3])
 
 @testset "Algopt.FirstOrder.MaximumGradientDescent" begin
     mgd = MaximumGradientDescent()
-    @test [-0.6, -0.8] == direction(mgd, ∇quad0, [3, 4])
-    @test [0, 0] == descent_step(mgd, quad0, ∇quad0, [3, 4])
+
+    @test [-0.6, -0.8] == direction_maxgd(∇quad0, [3, 4])
+    @test [0, -1] == direction_maxgd(∇quad3, [3, 4])
+
+    @test [0, 0] == descent_step_maxgd(quad0, ∇quad0, [3, 4])
+    @test [0, 0] == descent_step_maxgd(quad0, ∇quad0, [10, 10])
+    @test [3, 3] ≈ descent_step_maxgd(quad3, ∇quad3, [3, 4])
+    @test [3, 3] ≈ descent_step_maxgd(quad3, ∇quad3, [10, 10])
+
     @test [0, 0] == search(mgd, quad0, ∇quad0, [3, 4])
-
-    @test [0, 0] == descent_step(mgd, quad0, ∇quad0, [10, 10])
     @test [0, 0] == search(mgd, quad0, ∇quad0, [10, 10])
-
-    mgd = MaximumGradientDescent()
-    @test [0, -1] == direction(mgd, ∇quad3, [3, 4])
-    @test [3, 3] ≈ descent_step(mgd, quad3, ∇quad3, [3, 4])
     @test [3, 3] ≈ search(mgd, quad3, ∇quad3, [3, 4])
-
-    @test [3, 3] ≈ descent_step(mgd, quad3, ∇quad3, [10, 10])
     @test [3, 3] ≈ search(mgd, quad3, ∇quad3, [10, 10])
 end
 
 @testset "Algopt.FirstOrder.GradientDescent" begin
-    gd = GradientDescent(α = 1e-2)
-    @test [3, 3.98] ≈ descent_step(gd, quad3, ∇quad3, [3, 4])
-    @test norm([3, 3] - search(gd, quad3, ∇quad3, [3, 4])) < 1
+    α = 1e-2
+    gd = GradientDescent(α = α)
 
-    @test [9.86, 9.86] ≈ descent_step(gd, quad3, ∇quad3, [10, 10])
+    @test [3, 3.98] ≈ descent_step_gd(α, quad3, ∇quad3, [3, 4])
+    @test [9.86, 9.86] ≈ descent_step_gd(α, quad3, ∇quad3, [10, 10])
+
+    @test norm([3, 3] - search(gd, quad3, ∇quad3, [3, 4])) < 1
     @test norm([3, 3] - search(gd, quad3, ∇quad3, [10, 10])) < 1
 end
 
